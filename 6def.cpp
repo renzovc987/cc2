07cab638dfb8a057d415de6503e746a88eadbfe8
renzovc987 <38012028+renzovc987@users.noreply.github.com>
2018-04-11 21:26:30 -0500

    Add files via upload

---
+++ 6def.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+int tam(char*cad)
+{
+    int i=0,t=0;
+    while(*(cad+i)!='\0')
+    {
+        t++;
+        i++;
+    }
+    return t;
+}
+void copiar1(char*cad1,char*cad2)
+{
+    int tam1=0,i=0;
+    while(cad1[i]!='\0')
+    {
+        tam1++;
+        i++;
+    }
+    cad2=new char[tam1];
+    int j=0;
+    while(cad1[j]!=0)
+    {
+        cad2[j]=cad1[j];
+        j++;
+    }
+    cout<<cad2<<endl;
+}
+char*copiar2(char *cad1,char *cad2)
+{
+   int t;
+   t=tam(cad1);
+   cad2=new char[t];
+   while(*cad1!='\0')
+   {   char auxiliar=*cad1;
+       *cad2=auxiliar;
+       cad1++;
+       cad2++;
+   }
+   cad2-=t;
+   return cad2;
+
+}
+
+int main()
+{
+    char*cadena1,*cadena2;
+    cadena1=new char[100];
+    cin>>cadena1;
+    cadena2=new char[100];
+    cout<<"USANDO ARREGLOS"<<endl;
+    copiar1(cadena1,cadena2);
+    cout<<"SOLO PUNTEROS"<<endl;
+    cout<<copiar2(cadena1,cadena2)<<endl;;
+
+}
